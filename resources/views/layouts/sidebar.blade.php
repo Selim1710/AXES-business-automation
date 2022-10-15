3cc87096744ba07b752a4b54bd421408afc21a75
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-10-15 15:36:15 +0600

    minimize code

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="#">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
@@ -14,9 +14,9 @@
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{route("price-list")}}">Price List</a>
+                        <a class="nav-link" href="{{ route("price-list") }}">Price List</a>
 
-                        <a class="nav-link" href="{{route("expense-record")}}">Expenses Record</a>
+                        <a class="nav-link" href="{{ route("expense-record") }}">Expenses Record</a>
                         <a class="nav-link" href="#">Create Expenses</a>
                         <a class="nav-link" href="{{route("expenses-head")}}"> Expenses Head</a>
                         <a class="nav-link" href="{{route("add-expenses-head")}}">Add Expenses Head</a>
@@ -30,12 +30,23 @@
 
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-sidenav-light.html">Purchase Order</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Purchase Order Create</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Purchase Invoice</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Purchase Invoice Create</a>
-                        <a class="nav-link" href="layout-sidenav-light.html"> Purchase Return</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Purchase Return Create</a>
+                        <a class="nav-link" href="#">Purchase Order</a>
+                        <a class="nav-link" href="#">Purchase Order Create</a>
+                        <a class="nav-link" href="#">Purchase Invoice</a>
+                        <a class="nav-link" href="#">Purchase Invoice Create</a>
+                        <a class="nav-link" href="#"> Purchase Return</a>
+                        <a class="nav-link" href="#">Purchase Return Create</a>
+                    </nav>
+                </div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#inventory" aria-expanded="false" aria-controls="inventory">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                       inventory
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+
+                <div class="collapse" id="inventory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="#"> Branch </a>
                     </nav>
                 </div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
@@ -47,8 +58,8 @@
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="{{route('users.index')}}">All User</a>
                         <a class="nav-link" href="{{route('users.create')}}">Add User</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Roles</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Permissions</a>
+                        <a class="nav-link" href="#">Roles</a>
+                        <a class="nav-link" href="#">Permissions</a>
                     </nav>
                 </div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DailyProcess;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 /*
@@ -18,12 +19,12 @@ Route::get('/', function () {
 });
 
 
-Route::get('/price-list',[\App\Http\Controllers\DailyProcess:: class,'PriceList'])->name('price-list');
-Route::get('/expense-record',[\App\Http\Controllers\DailyProcess:: class,'expenseRecord'])->name('expense-record');
-Route::get('/expenses-head',[\App\Http\Controllers\DailyProcess:: class,'expensesHead'])->name('expenses-head');
-Route::get('/add-expenses-head',[\App\Http\Controllers\DailyProcess:: class,'AddExpensesHead'])->name('add-expenses-head');
+Route::get('/price-list',[DailyProcess:: class,'PriceList'])->name('price-list');
+Route::get('/expense-record',[DailyProcess:: class,'expenseRecord'])->name('expense-record');
+Route::get('/expenses-head',[DailyProcess:: class,'expensesHead'])->name('expenses-head');
+Route::get('/add-expenses-head',[DailyProcess:: class,'AddExpensesHead'])->name('add-expenses-head');
 
-Route::get('/priceblade',[\App\Http\Controllers\DailyProcess:: class,'PriceList'])->name('priceblade');
+Route::get('/priceblade',[DailyProcess:: class,'PriceList'])->name('priceblade');
 //
 Route::namespace('App\Http\Controllers')->group(function(){
 
