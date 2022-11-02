c134c7f04ca2cb8fee9b532d0f7af575f0f9def0
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-11-02 10:18:22 +0600

    transfer branch product showing

--- app/Http/Controllers/Backend/Inventory/TransferBranchController.php
+++ app/Http/Controllers/Backend/Inventory/TransferBranchController.php
@@ -4,43 +4,47 @@ namespace App\Http\Controllers\Backend\Inventory;
 
 use App\Http\Controllers\Controller;
 use App\Models\Inventory\Branch;
+use App\Models\ProductSetup\Product;
 use Illuminate\Http\Request;
 
 class TransferBranchController extends Controller
 {
-    
+
     public function index()
     {
-        $transBranchProducts = Branch::all();
-        return view('inventory.transfer_branch_table', compact('transBranchProducts'));
+        $branches = Branch::with('warehouse')->get();
+        $transferedBranches = Branch::all();
+        return view('inventory.transfer_branch_table', compact('branches','transferedBranches'));
     }
 
-    
+
     public function create()
     {
-        $branchProducts = Branch::all();
+        dd('create');
+    }
+
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $branchProducts = Product::where('branch_id', $request->branch_id)->get();
+        // dd($branchProducts);
         return view('inventory.create_branch_transfer', compact('branchProducts'));
     }
 
-   
-    public function store(Request $request)
-    {
-        //
-    }
 
-   
     public function show($id)
     {
-        //
+        dd($id);
     }
 
-    
+
     public function edit($id)
     {
         //
     }
 
-  
+
     public function update(Request $request, $id)
     {
         //
