10957adfa7204a885e28ee837e10f1636b7e4851
Saniatul Haque Taposh <taposhmd2@gmail.com>
2022-12-14 14:30:23 +0600

    s

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,11 +10,12 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
    public function dashboard5(){
-      
+
        return view('dashboard.index',[
           'total_customer'=> Customer::all()->count(),
            'total_purchase' => DB::table("purchase_orders")->sum('total'),
           'total_sales' => DB::table('sales_estimates')->sum('total'),
+          'total_expenses' => DB::table('invoices')->sum('totalamount'),
        ]);
    }
 }
