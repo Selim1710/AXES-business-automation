9e39002ef168194c53f529579891caf51bcdd944
Sohel Rana <sohelrananull@gmail.com>
2022-12-14 11:27:18 +0600

    sales total

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use App\Models\SalesEstimate;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
    public function dashboard5(){
+      
        return view('dashboard.index',[
           'total_customer'=> Customer::all()->count(),
+          'total_sales' => DB::table('sales_estimates')->sum('total'),
        ]);
    }
 }
