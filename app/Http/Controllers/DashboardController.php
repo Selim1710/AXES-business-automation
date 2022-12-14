0ec6bea5312a1cf289e1bfb739eed25288f3d90c
Saniatul Haque Taposh <taposhmd2@gmail.com>
2022-12-14 11:27:32 +0600

    total purchaes count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
    public function dashboard5(){
        return view('dashboard.index',[
           'total_customer'=> Customer::all()->count(),
+           'total_purchase' => DB::table("purchase_orders")->sum('total'),
        ]);
    }
 }
